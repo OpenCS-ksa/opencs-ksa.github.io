7949a924f18a9da42863ca127e477ee8658bd5c0
CLiF <ksa.opencs@gmail.com>
2023-02-24 19:49:41 +0900

    .

---
+++ python/theme 2/P1.py
@@ -0,0 +1,7 @@
+def average(a, b):
+    # ADD ADDITIONAL CODE HERE!
+    return (a + b) / 2
+
+print(average(8, 2))            # 5.0
+print(average(247, -893))       # -323.0
+print(average(0.837, 2.351))    # 1.594
